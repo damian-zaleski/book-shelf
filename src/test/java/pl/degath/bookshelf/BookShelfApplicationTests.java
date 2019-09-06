30a5edc880c0d916a2fcdd191bb615143b5e116a
degath <zaleskid1@gmail.com>
2019-09-06 14:43:38 +0200

    bs-001 update gradle build (add JUnit5)

--- src/test/java/pl/degath/bookshelf/BookShelfApplicationTests.java
+++ src/test/java/pl/degath/bookshelf/BookShelfApplicationTests.java
@@ -1,16 +1,13 @@
 package pl.degath.bookshelf;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class BookShelfApplicationTests {
+@ExtendWith(SpringExtension.class)
+class BookShelfApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
